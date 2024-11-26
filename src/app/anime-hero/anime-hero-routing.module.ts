8b23509b630fe37908d780130ad90b642fdaf866
jheisson  rojas <jfrojas592@misena.edu.co>
2024-11-26 14:35:49 -0500

    routes

--- src/app/anime-hero/anime-hero-routing.module.ts
+++ src/app/anime-hero/anime-hero-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+
+//localhost:4200/anime-heroe/''
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {
+  auth:
+  
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
