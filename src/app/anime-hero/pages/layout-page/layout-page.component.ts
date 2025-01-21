957d7ba8f667a2e38199726706404e0a07d3fafe
jheisson  rojas <jfrojas592@misena.edu.co>
2025-01-21 15:19:14 -0500

    cambios 21 de enero

--- src/app/anime-hero/pages/layout-page/layout-page.component.ts
+++ src/app/anime-hero/pages/layout-page/layout-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { url } from 'inspector';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.intefaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -13,5 +16,19 @@ export class LayoutPageComponent {
     {label: 'Añadir', icon: 'add', url: './new-hero' },
     {label: 'Buscar', icon: 'search', url: './search' },
   ]
+
+  constructor (
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+   get user():User | undefined {
+    return this.authService.currentUser;
+   }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {  Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 const routes: Routes = [
 
   // dominio.com/''
  {
     path:'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [PublicGuard],
+    canMatch: [PublicGuard]
  },
  {
     path:'hero',
-    loadChildren: () => import('./anime-hero/heroes.module').then(m => m.HeroModule)
+    loadChildren: () => import('./anime-hero/heroes.module').then(m => m.HeroModule),
+    canActivate:[AuthGuard],
+    canMatch: [AuthGuard],
  },
  {
     path:'404',
